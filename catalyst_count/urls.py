272aeefea6bb24d8cf2779e0bd13a24cf1db1b67
paramjeet101 <parammahato8454@gmail.com>
2024-12-17 19:46:56 +0530

    changes

--- catalyst_count/urls.py
+++ catalyst_count/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('api_part.urls'))
+    path('',include('api_part.urls'))  #added
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
